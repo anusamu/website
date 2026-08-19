66257fe24f0eccfa1fea9f091bcd66b1ff0638bb
anusamu <anusamu7202@gmail.com>
2026-08-19 17:17:14 +0530

    url added wholesale

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ connectDB();
 
 const allowedOrigins = [
   "http://localhost:5173", "http://localhost:5174",
-  process.env.CLIENT_URL,
+  process.env.CLIENT_URL, process.env.WHOLESALE_URL,
 ];
 
 console.log("CLIENT_URL:", process.env.CLIENT_URL);
